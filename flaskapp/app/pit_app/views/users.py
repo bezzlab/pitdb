9c56b727b2945d362c854922d38373d3a696d7fc
Elena <ea.chatzimichali@gmail.com>
2015-12-14 10:21:43 +0000

    New structure

---
+++ flaskapp/app/pit_app/views/users.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, render_template
+# Import the database object from the main app module
+from pit_app import db
+
+from pit_app.models import User
+
+users = Blueprint('users',  __name__)
+
+@users.route('/profile')
+def profile():
+  user = User.query.filter_by(address="test").first_or_404()
+    
+  if user == None:
+  	flash('User %s not found.' % user)
+  	return redirect(url_for('home.index'))
+  
+  return render_template('users/profile.html', user=user)
