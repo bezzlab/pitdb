fc844d089cbbe38a8707933b3b8af8dd53664ead
Elena <ea.chatzimichali@gmail.com>
2016-04-14 14:44:02 +0100

    View for static data

---
+++ flaskapp/app/pit_app/views/data.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, render_template, send_from_directory, Flask, send_file
+
+# app = Flask(__name__, static_folder='data/GFF3')
+# app = Flask(__name__, static_url_path='/data')
+
+data = Blueprint('data',  __name__)
+
+@data.route('/data/<filename>')
+def download_data(filename):
+  return send_file('static/data/'+filename)
