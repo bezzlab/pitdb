5a0389ac8438fe21df0ef29da8ef45d49c3289ab
Elena <ea.chatzimichali@gmail.com>
2016-05-27 18:17:21 +0100

    test

---
+++ config.py
@@ -0,0 +1,12 @@
+# Turns on debugging features in Flask
+DEBUG = True 
+
+# Configuration for the Flask-Bcrypt extension
+BCRYPT_LEVEL = 12 
+
+# For use in application emails
+MAIL_FROM_EMAIL = "me@me.com"
+
+SQLALCHEMY_DATABASE_URI = "postgresql://elena:3d0bb696@<pitdb.cqksmkzhuffl.eu-west-1.rds.amazonaws.com:5432>/pit"
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
