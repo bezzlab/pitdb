d391a0187bf8cbbce0a6ba4d660cdf1ee1103b91
Elena <ea.chatzimichali@gmail.com>
2016-06-01 15:40:30 +0100

    Adding search functionality

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ DEBUG = True
 BCRYPT_LEVEL = 12 
 
 # For use in application emails
-MAIL_FROM_EMAIL = "me@me.com" 
+MAIL_FROM_EMAIL = "me@me.com"
+
+SQLALCHEMY_DATABASE_URI = "postgresql://elena:3d0bb696@<pitdb.cqksmkzhuffl.eu-west-1.rds.amazonaws.com:5432>/pit"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
