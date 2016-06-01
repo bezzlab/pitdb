5114027b001f54bb0866b3b47b89aff1714e0a16
Elena <ea.chatzimichali@gmail.com>
2016-06-02 00:37:59 +0100

    Test

--- pit_app/__init__.py
+++ pit_app/__init__.py
@@ -21,8 +21,8 @@ def errorNew(error):
   return render_template('error/500.html'), 500
 
 from pit_app.views.home      import home
-from pit_app.views.search    import search
-from pit_app.views.results   import results
+# from pit_app.views.search    import search
+# from pit_app.views.results   import results
 # from pit_app.views.users     import users
 # from pit_app.views.data      import data
 # from pit_app.views.plots     import plots
@@ -30,8 +30,8 @@ from pit_app.views.results   import results
 
 # # Register blueprint(s)
 application.register_blueprint(home)
-application.register_blueprint(search)
-application.register_blueprint(results)
+# application.register_blueprint(search)
+# application.register_blueprint(results)
 # application.register_blueprint(users)
 # application.register_blueprint(data)
 # application.register_blueprint(plots)
