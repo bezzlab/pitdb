b65431fa366292acd63e67e932f52648e856d1dc
Elena <ea.chatzimichali@gmail.com>
2016-06-02 01:28:45 +0100

    Revert changes

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ BCRYPT_LEVEL = 12
 # For use in application emails
 # MAIL_FROM_EMAIL = "me@me.com"
 
-driver = 'postgresql+psycopg2://'
+# driver = 'postgresql+psycopg2://'
 
-SQLALCHEMY_DATABASE_URI = driver + os.environ['RDS_USERNAME'] + ':' + os.environ['RDS_PASSWORD'] \
-													+'@' + os.environ['RDS_HOSTNAME']  +  ':' + os.environ['RDS_PORT'] \
-													+ '/' + os.environ['RDS_DB_NAME']
+# SQLALCHEMY_DATABASE_URI = driver + os.environ['RDS_USERNAME'] + ':' + os.environ['RDS_PASSWORD'] \
+# 													+'@' + os.environ['RDS_HOSTNAME']  +  ':' + os.environ['RDS_PORT'] \
+# 													+ '/' + os.environ['RDS_DB_NAME']
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
