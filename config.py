f43eee8e86b9ff8ec80b2b5cdd54fb11334470f4
Elena <ea.chatzimichali@gmail.com>
2016-06-18 00:46:20 +0100

    More testing

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ if 'RDS_HOSTNAME' in os.environ:
 	SQLALCHEMY_DATABASE_URI = driver + os.environ['RDS_USERNAME'] + ':' + os.environ['RDS_PASSWORD'] \
 													+'@' + os.environ['RDS_HOSTNAME']  +  ':' + os.environ['RDS_PORT'] \
 													+ '/' + os.environ['RDS_DB_NAME']
-else:
-	BASE_DIR = os.path.abspath(os.path.dirname(__file__)) 
-	stream   = open(os.path.join(BASE_DIR, 'db.yml'), "r")
-	db       = yaml.load(stream)
-	stream.close()
+# else:
+# 	BASE_DIR = os.path.abspath(os.path.dirname(__file__)) 
+# 	stream   = open(os.path.join(BASE_DIR, 'db.yml'), "r")
+# 	db       = yaml.load(stream)
+# 	stream.close()
 	
-	SQLALCHEMY_DATABASE_URI = driver + db["test"]["username"]+":"+db["test"]["password"] \
-													+"@"+db["test"]["host"]+"/"+db["test"]["database"]
+# 	SQLALCHEMY_DATABASE_URI = driver + db["test"]["username"]+":"+db["test"]["password"] \
+# 													+"@"+db["test"]["host"]+"/"+db["test"]["database"]
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
