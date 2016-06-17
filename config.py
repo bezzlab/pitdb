fd39b0e3a83078ddb0599f0fd432dbfe7ebc4bfa
Elena <ea.chatzimichali@gmail.com>
2016-06-17 08:51:31 +0100

    Print msg for RDS

--- config.py
+++ config.py
@@ -13,20 +13,19 @@ BCRYPT_LEVEL = 12
 driver = 'postgresql+psycopg2://'
 
 if 'RDS_HOSTNAME' in os.environ:
+	print "connection details found"
 	SQLALCHEMY_DATABASE_URI = driver + os.environ['RDS_USERNAME'] + ':' + os.environ['RDS_PASSWORD'] \
 													+'@' + os.environ['RDS_HOSTNAME']  +  ':' + os.environ['RDS_PORT'] \
 													+ '/' + os.environ['RDS_DB_NAME']
 else:
 	print "can't find the environment variables"
-	# SQLALCHEMY_DATABASE_URI = driver + db["test"]["username"]+":"+db["test"]["password"] \
-	# 												+"@"+db["test"]["host"]+"/"+db["test"]["database"]
 
-	# BASE_DIR = os.path.abspath(os.path.dirname(__file__)) 
-	# stream   = open(os.path.join(BASE_DIR, 'db.yml'), "r")
-	# db       = yaml.load(stream)
-	# stream.close()
+	BASE_DIR = os.path.abspath(os.path.dirname(__file__)) 
+	stream   = open(os.path.join(BASE_DIR, 'db.yml'), "r")
+	db       = yaml.load(stream)
+	stream.close()
 	
-	# SQLALCHEMY_DATABASE_URI = driver + db["test"]["username"]+":"+db["test"]["password"] \
-	# 												+"@"+db["test"]["host"]+"/"+db["test"]["database"]
+	SQLALCHEMY_DATABASE_URI = driver + db["test"]["username"]+":"+db["test"]["password"] \
+													+"@"+db["test"]["host"]+"/"+db["test"]["database"]
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
