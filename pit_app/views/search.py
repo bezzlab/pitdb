98854f805374657a533166157062ee03f45c6177
Elena <ea.chatzimichali@gmail.com>
2016-08-12 14:18:22 +0300

    Gene name to gene symbol

--- pit_app/views/search.py
+++ pit_app/views/search.py
@@ -21,7 +21,7 @@ def advance():
     return redirect(url_for('results.peptide',  searchData = searchData.upper(), searchType=searchType))
   elif searchOption == 'Experiment ID':
     return redirect(url_for('results.experiment', experiment = searchData.upper()))
-  elif searchOption == 'Gene Name':
+  elif searchOption == 'Gene Symbol':
     return redirect(url_for('results.gene', gene = searchData))
   elif searchOption == 'Uniprot ID':
     return redirect(url_for('results.protein', uniprot = searchData.upper()))
